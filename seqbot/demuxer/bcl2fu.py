7388bd5c4b625f2277ba76b1577abdde24df3d6b
James Webber <james.webber@czbiohub.org>
2018-08-31 16:38:21 -0700

    pathlib.Path >> os.path added some helper functions to get the read header mostly right write_fastq is basically read_extraction but it writes the header and quality scores

--- seqbot/demuxer/bcl2fu.py
+++ seqbot/demuxer/bcl2fu.py
@@ -3,7 +3,7 @@
 import glob
 import gzip
 import itertools
-import os
+import pathlib
 import struct
 
 from collections import defaultdict, namedtuple, Counter
@@ -16,14 +16,14 @@ CBCL = namedtuple('CBCL', ('version', 'header_size', 'bits_per_basecall',
                            'number_of_tile_records', 'tile_offsets',
                            'non_PF_clusters_excluded'))
 
-get_cycle = lambda cfn: int(os.path.basename(os.path.dirname(cfn))[1:-2])
-get_part = lambda cfn: int(os.path.basename(cfn)[2])
-get_tile = lambda cfn: int(os.path.basename(cfn)[4:8])
+get_cycle = lambda cfn: int(cfn.parent.name[1:-2])
+get_part = lambda cfn: int(cfn.name[2])
+get_tile = lambda cfn: int(cfn.name[4:8])
 
 
 
 def read_cbcl_data(cbcl_files):
-    cbcl_file_data = dict()
+    cbcl_data = dict()
 
     for fn in cbcl_files:
         with open(fn, 'rb') as f:
@@ -40,17 +40,17 @@ def read_cbcl_data(cbcl_files):
 
             non_PF_clusters_excluded = bool(struct.unpack('B', f.read(1))[0])
 
-            cbcl_file_data[fn] = CBCL(version,
-                                      header_size,
-                                      bits_per_basecall,
-                                      bits_per_qscore,
-                                      number_of_bins,
-                                      bins,
-                                      number_of_tile_records,
-                                      tile_offsets,
-                                      non_PF_clusters_excluded)
+            cbcl_data[fn] = CBCL(version,
+                                 header_size,
+                                 bits_per_basecall,
+                                 bits_per_qscore,
+                                 number_of_bins,
+                                 bins,
+                                 number_of_tile_records,
+                                 tile_offsets,
+                                 non_PF_clusters_excluded)
 
-    return cbcl_file_data
+    return cbcl_data
 
 
 def read_cbcl_filters(filter_files):
@@ -66,44 +66,61 @@ def read_cbcl_filters(filter_files):
     return cbcl_filters
 
 
-def cbcl_globber(bcl_path):
+def read_cbcl_locs(loc_file):
+    with open(loc_file, 'rb') as f:
+        _, _, n_clusters = struct.unpack('<III', f.read(12))
+        cbcl_locs = (np.fromfile(
+                f, dtype=np.float32, count=2 * n_clusters
+        ) * 10 + 1000).reshape((n_clusters, 2)).round().astype(int)
+
+    return cbcl_locs
+
+
+def cbcl_id(bcl_path: pathlib.Path):
+    _, instrument, run_id, flowcell_id = bcl_path.name.split('_')
+
+    return f'@{instrument}:{int(run_id)}:{flowcell_id}'
+
+
+def cbcl_globber(bcl_path: pathlib.Path):
     cbcl_file_lists = dict()
     cbcl_filter_lists = dict()
 
     for lane in (1, 2, 3, 4):
         for part in itertools.count(1):
-            cbcl_files = glob.glob(
-                    os.path.join(bcl_path, 'Data', 'Intensities', 'BaseCalls',
-                                 'L00{}'.format(lane), 'C*.1',
-                                 'L00{}_{}.cbcl'.format(lane, part))
-            )
+            cbcl_files = sorted((
+                bcl_path / 'Data' / 'Intensities' / 'BaseCalls' / f'L00{lane}'
+            ).glob(f'C*.1/L00{lane}_{part}.cbcl'), key=get_cycle)
+
             if cbcl_files:
-                cbcl_files.sort(key=get_cycle)
                 cbcl_file_lists[lane, part] = cbcl_files
             else:
                 break
 
-        cbcl_filter_list = glob.glob(
-                os.path.join(bcl_path, 'Data', 'Intensities', 'BaseCalls',
-                             'L00{}'.format(lane),
-                             's_{}_*.filter'.format(lane))
-        )
+        cbcl_filter_list = sorted((
+            bcl_path / 'Data' / 'Intensities' / 'BaseCalls' / f'L00{lane}'
+        ).glob(f's_{lane}_*.filter'), key=get_tile)
 
-        cbcl_filter_list.sort(key=get_tile)
         cbcl_filter_lists[lane] = cbcl_filter_list
 
-    return cbcl_file_lists, cbcl_filter_lists
+    loc_file = bcl_path / 'Data' / 'Intensities' / 's.locs'
+    assert loc_file.exists()
+
+    return cbcl_file_lists, cbcl_filter_lists, loc_file
 
 
-def get_cbcl_data(cbcl_file_lists):
-    cbcl_data = read_cbcl_data(cbcl_file_lists)
+def get_cbcl_data(cbcl_file_list):
+    cbcl_data = read_cbcl_data(cbcl_file_list)
 
-    number_of_tiles = {cbcl_data[fn].number_of_tile_records
-                       for fn in cbcl_file_lists}
+    # assertion: the tile numbers are the same across all of these files
+    assert np.array_equal(
+        np.vstack([ci.tile_offsets[:,0] for ci in cbcl_data.values()]).max(0),
+        np.vstack([ci.tile_offsets[:,0] for ci in cbcl_data.values()]).min(0)
+    )
 
-    assert len(number_of_tiles) == 1
+    tiles = cbcl_data[cbcl_file_list[0]].tile_offsets[:,0]
 
-    return cbcl_data, number_of_tiles.pop()
+    return cbcl_data, tiles
 
 
 def get_byte_lists(cbcl_files, cbcl_data, cbcl_filter_data, tile_i):
@@ -117,17 +134,18 @@ def get_byte_lists(cbcl_files, cbcl_data, cbcl_filter_data, tile_i):
 
             try:
                 byte_array = np.frombuffer(
-                        gzip.decompress(f.read(ci.tile_offsets[tile_i, 3])),
-                        dtype=np.uint8, count=ci.tile_offsets[tile_i, 2]
+                    gzip.decompress(f.read(ci.tile_offsets[tile_i, 3])),
+                    dtype=np.uint8, count=ci.tile_offsets[tile_i, 2]
                 )
             except OSError:
                 yield (None, None)
                 continue
 
             ub = np.unpackbits(byte_array[::-1, None], axis=1).reshape((-1, 2))
+            pb = (np.packbits(ub, axis=1) >> 6).squeeze()[::-1]
 
-            base_array = (np.packbits(ub[1::2,:], axis=1) >> 6).squeeze()[::-1]
-            qscore_array = (np.packbits(ub[::2,:], axis=1) >> 6).squeeze()[::-1]
+            base_array = pb[::2]
+            qscore_array = pb[1::2]
 
             if not ci.non_PF_clusters_excluded:
                 yield base_array[cf], qscore_array[cf]
@@ -135,10 +153,10 @@ def get_byte_lists(cbcl_files, cbcl_data, cbcl_filter_data, tile_i):
                 yield base_array[:odd], qscore_array[:odd]
 
 
-def extract_reads(cbcl_files, cbcl_filter_files, i, nproc):
+def extract_reads(cbcl_files, cbcl_filter_files, loc_file, i, nproc):
     cbcl_filter_data = read_cbcl_filters(cbcl_filter_files)
-
-    cbcl_data, n_tiles = get_cbcl_data(cbcl_files)
+    cbcl_data, tiles = get_cbcl_data(cbcl_files)
+    cbcl_locs = read_cbcl_locs(loc_file)
 
     # retrieve qscore binning
     code_set = set(tuple(ci.bins[:,1] + 33) for ci in cbcl_data.values())
@@ -147,7 +165,10 @@ def extract_reads(cbcl_files, cbcl_filter_files, i, nproc):
     assert len(code_set) == 1
     code_set = bytes(code_set.pop()).decode() + '#'
 
-    for ii in range(i, n_tiles, nproc):
+    for ii in range(i, tiles.shape[0], nproc):
+        tile_no = tiles[ii]
+        cf = cbcl_filter_data[tile_no]
+
         ba_generator = enumerate(
             get_byte_lists(cbcl_files, cbcl_data, cbcl_filter_data, ii)
         )
@@ -170,4 +191,5 @@ def extract_reads(cbcl_files, cbcl_filter_files, i, nproc):
         yield from zip((''.join('ACGTN'[b] for b in base_matrix[k, :])
                         for k in range(base_matrix.shape[0])),
                        (''.join(code_set[b] for b in qscore_matrix[k, :])
-                        for k in range(qscore_matrix.shape[0])))
+                        for k in range(qscore_matrix.shape[0])),
+                       (f'{tile_no}:{r[0]}:{r[1]}' for r in cbcl_locs[cf,:]))
